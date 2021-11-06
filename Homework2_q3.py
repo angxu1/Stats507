35a36608e5ac5502fa1bc2279a8e015c0af54960
angxu1 <70584505+angxu1@users.noreply.github.com>
2021-11-05 20:19:57 -0400

    include “Gender”

--- Homework2_q3.py
+++ Homework2_q3.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 # ---
 # jupyter:
 #   jupytext:
@@ -14,14 +15,53 @@
 
 # # Packages
 
+import os
+import sys
 import re
-import time
 import numpy as np
 import pandas as pd
-from tabulate import tabulate
-from collections import defaultdict
+import matplotlib.pyplot as plt
+from Homework1 import mean_est, prop_est
+from scipy.stats import norm, ttest_ind, chi2_contingency
+from IPython.core.display import display, HTML
 
-# # Question 3
+# # Question 0 - Topics in Pandas
+
+# ##  Window Functions
+# * Provide the calculation of statistics 
+# * Primarily used in signal processing and time series data
+# * Perform desired mathematical operations on consecutive values at a time
+
+# ## Rolling Functions
+# * Can apply on Series and Dataframe type
+# * For DataFrame, each function is computed column-wise
+# * Here is an example for calculation of rolling sum
+# * Similar way to calculate the mean, var, std ...
+
+series = pd.Series(range(5))
+df = pd.DataFrame({"A": series, "B": series ** 3})
+df.rolling(3).sum()
+
+# ## Parameters
+# * **win_type** changes the window function (equally weighted by default)
+# * You can set the minimum number of observations in window required to have a
+# value by using **min_periods=k**
+# * Set the labels at the center of the window by using **center==True** 
+# (set to the right edge by default)
+
+print(df.rolling(3, win_type='gaussian').sum(std=3))
+print(df.rolling(3, win_type='gaussian', center=True).sum(std=3))
+print(df.rolling(3, win_type='gaussian', min_periods=2, 
+                 center=True).sum(std=3))
+
+# ## Expanding Functions
+# * Calculate the expanding sum of given DataFrame or Series
+# * Perform desired mathematical operations on current all previous values
+# * Similar way to calculate the mean, var, std...
+
+df.expanding(min_periods=2).sum()
+
+# # Question 1 - NHANES Table 1
 
 # part a
 demo_g = pd.read_sas('DEMO_G.XPT')
@@ -35,7 +75,7 @@ demo_j['cohort'] = '2017-2018'
 demo_data = pd.concat([demo_g,demo_h,demo_i,demo_j], ignore_index=True)
 demo_data = demo_data[['SEQN', 'RIDAGEYR', 'RIDRETH3', 'DMDEDUC2',
                        'DMDMARTL', 'RIDSTATR', 'SDMVPSU', 'SDMVSTRA',
-                       'WTMEC2YR', 'WTINT2YR', 'cohort']]
+                       'WTMEC2YR', 'WTINT2YR', 'cohort', 'RIAGENDR']]
 demo_data = demo_data.rename(columns={'SEQN': 'id', 'RIDAGEYR': 'age', 
                                       'RIDRETH3': 'race_and_ethnicity',
                                       'DMDEDUC2': 'education',
@@ -44,7 +84,8 @@ demo_data = demo_data.rename(columns={'SEQN': 'id', 'RIDAGEYR': 'age',
                                       'SDMVPSU': 'survey_weighting_var2',
                                       'SDMVSTRA': 'survey_weighting_var3',
                                       'WTMEC2YR': 'survey_weighting_var4',
-                                      'WTINT2YR': 'survey_weighting_var5'})
+                                      'WTINT2YR': 'survey_weighting_var5',
+                                      'RIAGENDR': 'gender'})
 demo_data = demo_data.astype({'id': 'int32', 'age': 'int32',
                               'education': 'category',
                               'marital_status': 'category',
@@ -52,38 +93,9 @@ demo_data = demo_data.astype({'id': 'int32', 'age': 'int32',
                               'survey_weighting_var1': 'category',
                               'survey_weighting_var2': 'category',
                               'survey_weighting_var3': 'int32',
-                              'cohort': 'category'})
-demo_data.to_pickle('demo_data_2011-2018.pkl')
+                              'cohort': 'category',
+                              'gender': 'category'})
+demo_data.to_pickle('demo_data_gender_2011-2018.pkl')
 
-# part b
-ohx_g = pd.read_sas('OHXDEN_G.XPT')
-ohx_g['cohort'] = '2011-2012'
-ohx_h = pd.read_sas('OHXDEN_H.XPT')
-ohx_h['cohort'] = '2013-2014'
-ohx_i = pd.read_sas('OHXDEN_I.XPT')
-ohx_i['cohort'] = '2013-2016'
-ohx_j = pd.read_sas('OHXDEN_J.XPT')
-ohx_j['cohort'] = '2017-2018'
-ohx_data = pd.concat([ohx_g, ohx_h, ohx_i, ohx_j], ignore_index=True)
-tc_cols = [col for col in ohx_data.columns 
-           if re.match(r"OHX[0-9][0-9]TC",col)]
-ctc_cols = [col for col in ohx_data.columns 
-            if re.match(r"OHX[0-9][0-9]CTC",col)]
-ohx_data = ohx_data[['SEQN', 'OHDDESTS']+tc_cols+ctc_cols]
-for i, tc_col_name in enumerate(tc_cols):
-    ohx_data = ohx_data.astype({tc_col_name: 'category'})
-    ohx_data = ohx_data.rename(columns={tc_col_name: 'tooth_counts_'+str(i)})
-for i, ctc_col_name in enumerate(ctc_cols):
-    ohx_data = ohx_data.astype({ctc_col_name: 'category'})
-    ohx_data = ohx_data.rename(columns={ctc_col_name:
-                                        'coronal cavities_'+str(i)})
-ohx_data = ohx_data.rename(columns={'SEQN': 'id',
-                                    'OHDDESTS': 'dentition_status_code'})
-ohx_data = ohx_data.astype({'id': 'int32',
-                            'dentition_status_code': 'category'})
-ohx_data.to_pickle('ohxden_2011-2018.pkl')
-
-print("There are {} cases in the dataset in part a".format(demo_data.shape[0]))
-print("There are {} cases in the dataset in part b".format(ohx_data.shape[0]))
 
 
