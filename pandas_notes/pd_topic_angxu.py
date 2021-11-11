b8e0cfb90d499e0bae433cd59f06b292af954cf9
angxu <70584505+angxu1@users.noreply.github.com>
2021-11-12 06:18:53 +0800

    Clone the starter script and add the topic notes to it

--- pandas_notes/pd_topic_angxu.py
+++ pandas_notes/pd_topic_angxu.py
@@ -1,29 +1,40 @@
-# -*- coding: utf-8 -*-
 # ---
 # jupyter:
 #   jupytext:
+#     cell_metadata_json: true
+#     notebook_metadata_filter: markdown
 #     text_representation:
 #       extension: .py
 #       format_name: light
 #       format_version: '1.5'
-#       jupytext_version: 1.13.0
+#       jupytext_version: 1.11.4
 #   kernelspec:
-#     display_name: Python 3
+#     display_name: Python 3 (ipykernel)
 #     language: python
 #     name: python3
 # ---
 
-# # Packages
+# ## Topics in Pandas
+# **Stats 507, Fall 2021** 
+#   
 
-import os
-import sys
-import re
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-from Homework1 import mean_est, prop_est
-from scipy.stats import norm, ttest_ind, chi2_contingency
-from IPython.core.display import display, HTML
+# ## Contents
+# Add a bullet for each topic and link to the level 2 title header using 
+# the exact title with spaces replaced by a dash. 
+#
+# + [Topic Title](#Topic-Title) 
+# + [Topic 2 Title](#Topic-2-Title)
+
+# ## Topic Title
+# Include a title slide with a short title for your content.
+# Write your name in *bold* on your title slide. 
+
+
+# ## Moving functions 
+# **Ang Xu**
+# + [Window Functions](#Window-Functions) 
+# + [Rolling Functions](#Rolling-Functions) 
+# + [Expanding Functions](#Expanding-Functions) 
 
 # # Question 0 - Topics in Pandas
 
